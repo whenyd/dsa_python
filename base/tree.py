3b38713b1ebd11fc224ec27630f45357a06d2c1f
yangdong <yangdong@zhiyuanhui.com>
2019-12-05 14:25:46 +0800

    [fea]不使用abc模块

--- base/tree.py
+++ base/tree.py
@@ -1,54 +1,47 @@
-from abc import ABC
-from abc import abstractmethod
+"""
+Abstract base class of tree ADT.
+"""
 
 
-class Tree(ABC):
+class Tree:
     """树的抽象基类."""
 
-    class Position(ABC):
+    class Position:
         """树中结点的位置ADT."""
 
-        @abstractmethod
         def element(self):
             """返回存储在当前Position的元素."""
-            pass
+            raise NotImplementedError('must be implemented by subclass')
 
-        @abstractmethod
         def __eq__(self, other):
-            pass
+            raise NotImplementedError('must be implemented by subclass')
 
         def __ne__(self, other):
             return not self == other
 
-    @abstractmethod
     def root(self):
         """返回root的Position, 空树返回None."""
-        pass
+        raise NotImplementedError('must be implemented by subclass')
 
-    @abstractmethod
     def parent(self, p):
         """返回p的父结点的Position, p为root返回None."""
-        pass
+        raise NotImplementedError('must be implemented by subclass')
 
-    @abstractmethod
     def children(self, p):
         """Generate an iteration of Positions representing p s children."""
-        pass
+        raise NotImplementedError('must be implemented by subclass')
 
-    @abstractmethod
     def num_children(self, p):
         """Return the number of children that Position p has."""
-        pass
+        raise NotImplementedError('must be implemented by subclass')
 
-    @abstractmethod
     def positions(self):
         """Generate an iteration of all positions of tree T."""
-        pass
+        raise NotImplementedError('must be implemented by subclass')
 
-    @abstractmethod
     def __len__(self):
         """Return the total number of elements in the tree."""
-        pass
+        raise NotImplementedError('must be implemented by subclass')
 
     def is_root(self, p):
         return self.root() == p
